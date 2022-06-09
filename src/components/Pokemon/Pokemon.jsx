86ecc765a45de4197f8a3830308de3d0507b649c
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-06-09 11:02:02 +0300

    add size props to Pokemon component

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Image, View } from "react-native";
 import DefaultText from "@Components/Text";
 import { useFonts, Roboto_700Bold } from "@expo-google-fonts/roboto";
-import styled from "@emotion/native";
+import styled, { css } from "@emotion/native";
 import zeropad from "zeropad";
 import { POKEMON_IMAGE_URL } from "src/constants/urls";
 
@@ -14,12 +14,11 @@ const Container = styled.View`
 `;
 
 const Text = styled(DefaultText)`
-  font-size: 10px;
   font-family: "Roboto_700Bold";
   text-align: center;
 `;
 
-export default function Pokemon(props) {
+export default function Pokemon({ id, name, fontSize, size }) {
   const [fontsLoaded] = useFonts({ Roboto_700Bold });
   if (!fontsLoaded) {
     return null;
@@ -28,17 +27,28 @@ export default function Pokemon(props) {
   return (
     <Container>
       <Image
-        style={{ height: 56, width: 56 }}
+        style={{ height: size, width: size }}
         resizeMode="cover"
-        source={{ uri: `${POKEMON_IMAGE_URL}/${zeropad(props.id, 3)}.png` }}
+        source={{ uri: `${POKEMON_IMAGE_URL}/${zeropad(id, 3)}.png` }}
       />
-      <Text color="#264653">{format(props.id)}</Text>
-      <Text color="#264653">{props.name}</Text>
+      <Text color="#264653" style={{ fontSize }}>
+        {format(id)}
+      </Text>
+      <Text color="#264653" style={{ fontSize }}>
+        {name}
+      </Text>
     </Container>
   );
 }
 
+Pokemon.defaultProps = {
+  fontSize: 10,
+  size: 56,
+};
+
 Pokemon.propTypes = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
+  fontSize: PropTypes.number,
+  size: PropTypes.number,
 };
--- storybook/stories/Pokemon/Pokemon.stories.js
+++ storybook/stories/Pokemon/Pokemon.stories.js
@@ -5,8 +5,21 @@ import CenterView from "../CenterView";
 
 storiesOf("Pokemon", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add("Charmander", () => <Pokemon id={4} name='Charmander' />)
-  .add("Charmeleon", () => <Pokemon id={5} name='Charmeleon' />)
-  .add("Charizard", () => <Pokemon id={6} name='Charizard' />)
-  .add("Drowzee", () => <Pokemon id={96} name='Drowzee' />)
-  .add("Hypno", () => <Pokemon id={97} name='Hypno' />);
+  .add("Pikachu", () => (
+    <Pokemon id={25} name="Pikachu" fontSize={15} size={200} />
+  ))
+  .add("Charmander", () => (
+    <Pokemon id={4} name="Charmander" fontSize={15} size={200} />
+  ))
+  .add("Charmeleon", () => (
+    <Pokemon id={5} name="Charmeleon" fontSize={15} size={200} />
+  ))
+  .add("Charizard", () => (
+    <Pokemon id={6} name="Charizard" fontSize={15} size={200} />
+  ))
+  .add("Drowzee", () => (
+    <Pokemon id={96} name="Drowzee" fontSize={15} size={200} />
+  ))
+  .add("Hypno", () => (
+    <Pokemon id={97} name="Hypno" fontSize={15} size={200} />
+  ));
